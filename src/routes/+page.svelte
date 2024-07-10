73ec1b368045848b4eef7df52272eb2147298fab
John Feusi <>
2024-07-10 16:58:07 -0600

    Added minimal reproduction code

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+  // import "@esri/calcite-components/dist/calcite/calcite.css";
+  import "@esri/calcite-components/dist/components/calcite-button";
+</script>
+<h1>Welcome to the Reproduction!</h1>
+<calcite-button>Click Me</calcite-button>
---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	plugins: [sveltekit()],
+	ssr: {
+    noExternal: [
+      '@esri/calcite-components',
+      '@stencil/core',
+    ],
+  }
+});
