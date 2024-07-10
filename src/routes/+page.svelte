143b27a62b64d757eac906cc3cd4488403338a59
John Feusi <>
2024-07-10 17:05:38 -0600

    Added `setAssetPath`

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script>
-  // import "@esri/calcite-components/dist/calcite/calcite.css";
+  import "@esri/calcite-components/dist/calcite/calcite.css";
+  import { setAssetPath } from "@esri/calcite-components/dist/components";
+  setAssetPath('/node_modules/@esri/calcite-components/dist/assets')
+  
   import "@esri/calcite-components/dist/components/calcite-button";
 </script>
 <h1>Welcome to the Reproduction!</h1>
